c042bd15be08b24e7254a7ed4aa9b2c8a6235f2f
oguzmarc14 <oguzmarc14@gmail.com>
2025-06-24 16:11:43 -0600

    feat(settings): install astro-show-tailwindcss-breakpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react";
 
 
+import showTailwindcssBreakpoint from "astro-show-tailwindcss-breakpoint";
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), showTailwindcssBreakpoint()],
 })
